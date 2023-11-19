3257ff46d9dee65d2c64594407f401057192266f
Klebinhop <120269841+Klebinhop@users.noreply.github.com>
2023-11-19 20:24:51 -0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,34 @@
+# $1 for ip
+# $2 for port
+# $3 for pairing code
+# $4 for adress
+unzip wwwfactory.zip
+cd wwwfactory
+echo "package com.wwwfactory
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val builder = CustomTabsIntent.Builder()
+        builder.setInstantAppsEnabled(true)
+        val customBuilder = builder.build()
+        customBuilder.intent.setPackage('com.android.chrome')
+        customBuilder.launchUrl(this, Uri.parse('$4'))
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        finishAndRemoveTask();
+    }
+
+}" > app/src/main/java/com/wwwfactory/MainActivity.kt
+adb pair "$1:$2 $3"
+./gradlew build
+./gradlew installDebug
