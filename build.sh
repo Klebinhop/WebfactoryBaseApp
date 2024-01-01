a4a47f87b395d677438eec96435dcc48d69745a2
Klebinhop <120269841+Klebinhop@users.noreply.github.com>
2024-01-01 12:47:20 -0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 # $1 for Web Adress
 # $2 for Site Name
 
-am start com.webfactory/.MainActivity
 pkg install wget aapt2 android-tools 7zip tsu graphicsmagick which openjdk-17 -y
 
 wget https://github.com/Lzhiyong/termux-ndk/releases/download/android-sdk/android-sdk-aarch64.zip
